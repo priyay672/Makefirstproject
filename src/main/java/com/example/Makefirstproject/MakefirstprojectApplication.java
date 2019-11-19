9c609e8f943d2c64a1510ddf30f0014a02956971
priya672 <priya672@gmail.com>
2019-11-19 10:54:07 +0530

    coderefactory

---
+++ src/main/java/com/example/Makefirstproject/MakefirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Makefirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakefirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakefirstprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Makefirstproject/MakefirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Makefirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakefirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
